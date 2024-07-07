ad2efe2f224e445cd221e202cbd49e989c7f94b3
Curt Mathias Rosenblad-Wheeler <curt.m.k.wheeler@icloud.com>
2024-07-06 20:00:37 -0400

    Slices

--- Slices/main.go
+++ Slices/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	f "fmt"
+)
 
 func main() {
-	fmt.Println()
+	var intSlice []int32 = []int32{4, 5, 6}
+	f.Printf("The length is %v with capacity %v.\n", len(intSlice), cap(intSlice))
+	f.Println(intSlice)
+
+	intSlice = append(intSlice, 7)
+	f.Printf("The length is %v with capacity %v.\n", len(intSlice), cap(intSlice))
+	f.Println(intSlice)
+
+	f.Println(intSlice[0:3])
+	f.Println(&intSlice[3])
+
+	var intSlice2 []int32 = []int32{8, 9}
+	intSlice = append(intSlice, intSlice2...)
+	f.Printf("The length is %v with capacity %v.\n", len(intSlice), cap(intSlice))
+	f.Println(intSlice)
 }
